e5fcc8894448752c25b1ea2fa68c02dc8b03ba2f
Apple <apple@Apples-Mac-mini.local>
2025-03-31 12:27:20 +0530

    create xcFramework

---
+++ ColorNameGenerator.xcframework/ios-arm64/ColorNameGenerator.framework/Headers/ColorNameGenerator.h
@@ -0,0 +1,18 @@
+//
+//  ColorNameGenerator.h
+//  ColorNameGenerator
+//
+//  Created by Apple on 31/03/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ColorNameGenerator.
+FOUNDATION_EXPORT double ColorNameGeneratorVersionNumber;
+
+//! Project version string for ColorNameGenerator.
+FOUNDATION_EXPORT const unsigned char ColorNameGeneratorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ColorNameGenerator/PublicHeader.h>
+
+
